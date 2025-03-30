0c52d927642615319fb3b6d1901de8dc63841d20
Anthony Altieri <anthonyaltieri0@gmail.com>
2025-03-29 23:00:32 -0400

    hunter made me scrao the grid :(

--- globals.py
+++ globals.py
@@ -1,10 +1,10 @@
 import json
 
-#load configuration from file
+# load configuration from file
 with open("config.json", "r") as f:
     config = json.load(f)
 
-#global configuration variables
+# global configuration variables
 TOKEN = config["testing_token"]
 GUILD_ID = int(config["testing_guild_id"])
 TARGET_MEMBER_ID = int(config["target_member_id"])
@@ -13,14 +13,13 @@ DATA_FILE = "data.json"
 ALLOWED_ROLES = ["him"]
 STOCK_FILE = "stocks.json"
 STOCK_HISTORY_FILE = "stock_history.json"
-UPDATE_INTERVAL_MINUTES = 20 
+UPDATE_INTERVAL_MINUTES = 20
 LOTTERY_FILE = "lottery.json"
 OPTIONS_FILE = "options.json"
 AFK_CHANNEL_ID = 574668552557297666
 
 
-#rpg globals
-RPG_INVENTORY_FILE = "rpg.json"
+# rpg globals
+RPG_INVENTORY_FILE = "./rpg.json"
 RPG_ITEMS_FILE = "rpgitems.json"
 RPG_PARTIES_FILE = "rpgparties.json"
-
--- rpgcombat.py
+++ rpgcombat.py
@@ -3,82 +3,112 @@ from discord import app_commands
 from discord.ext import commands
 from globals import RPG_INVENTORY_FILE, GUILD_ID
 from rpgutils import (
+    check_user_in_party,
+    get_party_data,
     rpg_load_data,
-    rpg_save_data,
-    update_equipment_bonuses_for_user,
-    calculate_starting_hp_mana_stamina,
-    full_heal,
+    roll_d12,
 )
 import enum
+import random
 
 
-class Tile(enum.Enum):
-    EMPTY = "🔳"
-    DEAD = "💀"
+class CombatTurnManager:
+    def __init__(self, player_ids: list[str]):
+        player_data = rpg_load_data()
+        print(player_data)
+        self.turn_order = [
+            {
+                "id": player_id,
+                "initiative": player_data[player_id]["speed"] + roll_d12(),
+            }
+            for player_id in player_ids
+        ]
+        self.turn_order = sorted(self.turn_order, key=lambda x: x["id"], reverse=True)
+        self.current_player_id = self.turn_order[0]["id"]
+        print(self.turn_order)
+
+    def next_turn(self):
+        """Set current player to next player in turn order, with circular looping"""
+        current_player_index = self.turn_order.index(self.current_player_id)
+        if current_player_index + 1 >= len(self.turn_order):
+            self.current_player_id = self.turn_order[0]
+        else:
+            self.current_player_id = self.turn_order[current_player_index + 1]
 
 
-class State(enum.Enum):
-    WAITING = "Waiting for player"
-    ENEMY_TURN = "Enemy's turn"
-
-
-class CombatScreen(discord.ui.View):
-    def __init__(self, grid_width=2):
+class CombatView(discord.ui.View):
+    def __init__(self, owner_id: str, members: list[str], is_party: bool):
         super().__init__()
-        # Create the grid first
-        self.grid_width = grid_width
-        self.grid = self.__create_grid()
-        # Create the embedding second
         self.embed = self.__create_embed()
-        # State machine
-        self.sate = State.WAITING
-        pass
+        # Get owner and current player from members lsit
+        self.owner_id = owner_id
+        self.combat_turn_manager = CombatTurnManager(members)
 
     def __create_embed(self):
         embed = discord.Embed(
             title="Combat Encounter",
-            description=f"You have encountered enemies!\n\n{self.__display_grid()}",
+            description=f"You have encountered enemies!\n\n",
             color=discord.Color.red(),
         )
         return embed
-
-    def __create_grid(self):
-        grid = [[Tile.EMPTY.value for _ in range(self.grid_width)] for _ in range(2)]
-        return grid
-
-    def __display_grid(self) -> str:
-        # Add padding, you better be thankful I didnt use numpy for this
-        grid_with_spacing = [[""] + [str(i) for i in range(self.grid_width)]] + [
-            [str(idx)] + row for idx, row in enumerate(self.grid)
-        ]
-        # Add column and row numbers to the grid because I know people wont be able to zero index
-        for i in range(len(grid_with_spacing[0])):
-            grid_with_spacing[0][i] = " " + str(i - 1) if i != 0 else ""
-        for i in range(len(grid_with_spacing)):
-            grid_with_spacing[i][0] = str(i - 1) if i != 0 else "-"
-        grid_with_spacing = "\n\n\n".join(["\t".join(row) for row in grid_with_spacing])
-
-        return f"```\n{grid_with_spacing}\n```"
-
-    def get_embed(self):
-        return self.embed
-
-    def update_grid(self, row, col, value) -> None:
-        self.grid[row][col] = value
-        self.embed = self.__create_embed()
-
-    def get_status(self) -> str:
-        return self.sate.value
 
 
 class CombatCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.combat_session = None
+        self.combat_sessions: dict[str, CombatView] = {}
 
     @app_commands.guilds(discord.Object(id=GUILD_ID))
     @app_commands.command(name="start_combat", description="Start a combat encounter.")
     async def start_combat(self, interaction: discord.Interaction):
+        local_party_data = get_party_data(str(interaction.user.id))
+        # Check / get already existing combat session for user or party leader
+        if str(interaction.user.id) in self.combat_sessions.keys():
+            local_combat_session = self.combat_sessions[str(interaction.user.id)]
+            await interaction.response.send_message(
+                "Ongoing combat session detected!",
+                embed=local_combat_session.embed,
+                view=local_combat_session,
+            )
+            return
+        elif local_party_data.get("leader", "") in self.combat_sessions.keys():
+            local_combat_session = self.combat_sessions[str(interaction.user.id)]
+            await interaction.response.send_message(
+                "Ongoing combat session detected!",
+                embed=local_combat_session.embed,
+                view=local_combat_session,
+            )
+            return
+
+        # No existing combat session, check if user is party leader or solo
+        if check_user_in_party(str(interaction.user.id)) == False:
+            local_combat_session = self.combat_sessions.get(
+                str(interaction.user.id),
+                CombatView(str(interaction.user.id), [str(interaction.user.id)], True),
+            )
+            self.combat_sessions[str(interaction.user.id)] = local_combat_session
+            await interaction.response.send_message(
+                "Solo combat started!!",
+                embed=local_combat_session.embed,
+                view=local_combat_session,
+            )
+        elif check_user_in_party(str(interaction.user.id), True) == True:
+            local_combat_session = self.combat_sessions.get(
+                str(interaction.user.id),
+                CombatView(str(interaction.user.id), local_party_data["members"], True),
+            )
+            self.combat_sessions[str(interaction.user.id)] = local_combat_session
+            await interaction.response.send_message(
+                "Party combat started!!",
+                embed=local_combat_session.embed,
+                view=local_combat_session,
+            )
+        else:
+            await interaction.response.send_message(
+                "You must run solo or be the leader of a party to start a combat."
+            )
+        return
+
         if self.combat_session == None:
             combat_screen = CombatScreen(grid_width=3)
             # This is where you can populate the grid with enemies
